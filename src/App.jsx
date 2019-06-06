e1d5cf1c8f00cb8f697e344501cd50db2b097b0c
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-06-06 00:16:11 +0000

    connect to server

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,61 @@ import { generateRandomId } from "./randomId.js";
 
 
 export default class App extends Component {
- constructor(props) {
+  constructor(props) {
     super(props);
+    this.webSocket = null
     this.state =
-      {
-    currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-    messages: [
-      {
-        id: 1,
-        username: "Bob",
-        content: "Has anyone seen my marbles?",
-      },
-      {
-        id: 2,
-        username: "Anonymous",
-        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-      }
-    ]
+    {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: []
     };
 
     this.addNewMessage = this.addNewMessage.bind(this);
   }
+
+
+ componentDidMount(){
+  var webSocket = new WebSocket("ws://localhost:3001");
+  webSocket.onmessage = (event) => {
+    const message = JSON.parse(event.data)
+    const messages = this.state.messages.concat(message)
+    this.setState({
+      messages: messages
+    })
+  }
+  this.webSocket = webSocket
+ }
+
+
+
+
+  addNewMessage(messageText) {
+    const newMessage = {
+      id: generateRandomId(),
+      username: "Bob",
+      content: messageText
+    };
+
+    this.webSocket.send(JSON.stringify(newMessage))
+   }
+
+  render() {
+    return (
+      <div>
+        <MessageList messages={this.state.messages}/>
+        // <ChatBar onKeyPress={this.keyDown} currentUser={this.state.currentUser.name}/>
+        <ChatBar onMessageSend={this.addNewMessage} currentUser={this.state.currentUser.name}/>
+
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
 
 
 // keyDown(event){
@@ -50,30 +84,4 @@ export default class App extends Component {
 
 //   }
 // }
-
-  addNewMessage(messageText) {
-    const newMessage = {
-      id: generateRandomId(),
-      username: "Bob",
-      content: messageText
-    };
-
-    const messages = this.state.messages.concat(newMessage)
-
-    this.setState({
-      messages: messages,
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <MessageList messages={this.state.messages}/>
-        // <ChatBar onKeyPress={this.keyDown} currentUser={this.state.currentUser.name}/>
-        <ChatBar onMessageSend={this.addNewMessage} currentUser={this.state.currentUser.name}/>
-
-      </div>
-    );
-  }
-}
 
